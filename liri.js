993dc536b457251d7a3d495eeb20f3c8c54a8700
zwarnerduncan <zwarnerduncan@yahoo.com>
2019-06-26 14:05:34 -0400

    enter

--- liri.js
+++ liri.js
@@ -1,3 +1,7 @@
 require("dotenv").config();
+
+var fs = require("fs")
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+var request = require("request");
