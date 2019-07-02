6e6aa600d451aa6633440d0fe3692f1f22b93dd0
zwarnerduncan <zwarnerduncan@yahoo.com>
2019-07-02 19:41:47 -0400

    updating read me and fixed syntzx error in liri.js, removed an extra {

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # liri-node-app
 1. The problem the app is trying to solve.
+
    LIRI is a Language Interpretation and Recognition Interface. This app is a command line node app that follows commands by retreiving parameters and out putting data. It does this because reqeusts are being made from different functionalities that need to be read and fectched in order for the commands to run properly.
 
 2. How the app is organized.
+
    The app is organized by 4 commands:
    -concert-this
    -spotify-this
@@ -11,4 +13,15 @@
    Each command gives data back that has to do specifically to the command. (i.e. command:concert-this returns data by the user inserting a band and the data comes back shows name of the event venue, date of the event, and venue location.)
 
 3. How to use LIRI
-   
+
+   1. Open your Terminal (i.e. Bash). 
+   2. Navigate to your liri.js file.
+   3. Pick any command to run and based on the command it will return different data.
+    
+    Example 1: Run the concert-this command
+
+    node liri.js concert-this <name of artist or band>
+
+    The system will display the name of the venue, the date of the event, and the location. 
+
+    
--- liri.js
+++ liri.js
@@ -21,7 +21,6 @@ function userOptions(userInput,inputParameter){
     case 'concert-this':
         showConcertInfo(inputParameter);
         break;
-
     case 'spotify-this-song':
         showSongInfo(inputParameter);
         break;
@@ -37,26 +36,26 @@ function userOptions(userInput,inputParameter){
 }
 
 //Function for Bands in Town//
-function showConcertInfo(inputParameter);{
-    var queryURL = "https:rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+function showConcertInfo(inputParameter){
+    var queryURL = "https://rest.bandsintown.com/artists/" + inputParameter + "/events?app_id=codingbootcamp";
     request(queryURL, function(error, response, body){
         //successful//
-        if (! error && response.statusCode ===200) {
-            var concerts = JSONparse(body);
-            for (var i = 0; i < concerts.lenghth; i++){
+        if (!error && response.statusCode ===200) {
+            var concerts = JSON.parse(body);
+            for (var i = 0; i < concerts.length; i++){
                 console.log ("*****************EVENT INFO*******************");
                 //appending in log.txt file//
-                fs.appendFileSync("log.txt", "*****************EVENT INFO*******************\n");
-                console.log(i);
-                fs.appendFileSync("log.txt", i+"\n")
-                console.log("Name of the venue" + concerts[i].venue.name);
-                fs.appendFileSync("log.txt","Name of the venue: ", concerts.venue.name +"\n");
+                // fs.appendFileSync("log.txt", "*****************EVENT INFO*******************\n");
+                // console.log(i);
+                // fs.appendFileSync("log.txt", i+"\n")
+                console.log("Name of the venue: " + concerts[i].venue.name);
+                // fs.appendFileSync("log.txt","Name of the venue: ", concerts.venue.name +"\n");
                 console.log("Venue location: " + concerts[i].venue.city);
-                fs.appendFileSync("log.txt","Venue location: ", concerts.venue.city +"\n");
+                // fs.appendFileSync("log.txt","Venue location: ", concerts.venue.city +"\n");
                 console.log("Date of event: ", concerts[i].datetime);
-                fs.appendFileSync("log.txt","Date of event: " + concerts.datetime+"\n")
+                // fs.appendFileSync("log.txt","Date of event: " + concerts.datetime+"\n")
                 console.log("***********************");
-                fs.appendFileSync("log.txt","***********************" +"\n")
+                // fs.appendFileSync("log.txt","***********************" +"\n")
             }
 
         }else{
@@ -65,3 +64,105 @@ function showConcertInfo(inputParameter);{
     }
 )};
 
+// //Funtion for Music Info: Spotify//
+function showSongInfo(inputParameter) {
+    if (inputParameter === undefined) {
+        inputParameter = "I Want it That Way"; //default Song
+    }
+    spotify.search(
+        {
+            type: "track",
+            query: inputParameter
+        },
+        function (err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
+                return;
+            }
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log("**********SONG INFO*********");
+                fs.appendFileSync("log.txt", "**********SONG INFO*********\n");
+                // console.log(i);
+                fs.appendFileSync("log.txt", i +"\n");
+                console.log("Song name: " + songs[i].name);
+                fs.appendFileSync("log.txt", "song name: " + songs[i].name +"\n");
+                console.log("Preview song: " + songs[i].preview_url);
+                fs.appendFileSync("log.txt", "preview song: " + songs[i].preview_url +"\n");
+                console.log("Album: " + songs[i].album.name);
+                fs.appendFileSync("log.txt", "album: " + songs[i].album.name + "\n");
+                console.log("Artist(s): " + songs[i].artists[0].name);
+                fs.appendFileSync("log.txt", "artist(s): " + songs[i].artists[0].name + "\n");
+                console.log("*****************************");  
+                fs.appendFileSync("log.txt", "*****************************\n");
+             }
+        }
+    );
+};
+
+
+//Funtion for Movie Info: OMDB//
+function showMovieInfo(inputParameter){
+    if (inputParameter === undefined) {
+        inputParameter = "Mr. Nobody"
+        console.log("-----------------------");
+        fs.appendFileSync("log.txt", "-----------------------\n");
+        console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+        fs.appendFileSync("log.txt", "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/" +"\n");
+        console.log("It's on Netflix!");
+        fs.appendFileSync("log.txt", "It's on Netflix!\n");
+}   }
+
+var queryUrl =  "https://www.omdbapi.com/?t=" + inputParameter + "&y=&plot=short&tomatoes=true&apikey=trilogy";
+    request(queryUrl, function(error, response, body) {
+    // If successful//
+    if (!error && response.statusCode === 200) {
+        var movies = JSON.parse(body);
+        console.log("**********MOVIE INFO*********");  
+        fs.appendFileSync("log.txt", "**********MOVIE INFO*********\n");
+        console.log("Title: " + movies.Title);
+        fs.appendFileSync("log.txt", "Title: " + movies.Title + "\n");
+        console.log("Release Year: " + movies.Year);
+        fs.appendFileSync("log.txt", "Release Year: " + movies.Year + "\n");
+        console.log("IMDB Rating: " + movies.imdbRating);
+        fs.appendFileSync("log.txt", "IMDB Rating: " + movies.imdbRating + "\n");
+        console.log("Rotten Tomatoes Rating: " + getRottenTomatoesRatingValue(movies));
+        fs.appendFileSync("log.txt", "Rotten Tomatoes Rating: " + getRottenTomatoesRatingValue(movies) + "\n");
+        console.log("Country of Production: " + movies.Country);
+        fs.appendFileSync("log.txt", "Country of Production: " + movies.Country + "\n");
+        console.log("Language: " + movies.Language);
+        fs.appendFileSync("log.txt", "Language: " + movies.Language + "\n");
+        console.log("Plot: " + movies.Plot);
+        fs.appendFileSync("log.txt", "Plot: " + movies.Plot + "\n");
+        console.log("Actors: " + movies.Actors);
+        fs.appendFileSync("log.txt", "Actors: " + movies.Actors + "\n");
+        console.log("*****************************");  
+        fs.appendFileSync("log.txt", "*****************************\n");
+    } else{
+      console.log('Error occurred.');
+    }
+
+// });
+
+// //function for Rotten Tomatoes Rating
+function getRottenTomatoesRatingObject (data) {
+    return data.Ratings.find(function (item) {
+       return item.Source === "Rotten Tomatoes";
+    });
+  }
+  
+  function getRottenTomatoesRatingValue (data) {
+    return getRottenTomatoesRatingObject(data).Value;
+  }
+
+// function for reading out of random.txt file  
+function showAnythingInfo(){
+	fs.readFile('random.txt', 'utf8', function(err, data){
+		if (err){ 
+			return console.log(err);
+		}
+        var dataArr = data.split(',');
+        userOptions(dataArr[0], dataArr[1]);
+	});
+}})
